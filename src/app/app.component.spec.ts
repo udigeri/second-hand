f1bfef03de01a73e4a8857d57d572d11c50a76d7
Hudak.Pavol <hudak.pavol@scheidt-bachmann.sk>
2023-04-04 12:44:11 +0200

    clean html and repair tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sh'`, () => {
+  it(`should have as title 'Najlepsa shopa v okolici'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sh');
+    expect(app.title).toEqual('Najlepsa shopa v okolici');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sh app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Najlepsa shopa v okolici');
   });
 });
